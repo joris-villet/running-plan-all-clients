3a7bfce342e6e50a294d7a8354bd00439ee86b4e
joris-villet <jorisvillet@gmail.com>
2022-05-07 21:04:30 +0200

    route login && background

--- svelte/src/pages/Dashboard.svelte
+++ svelte/src/pages/Dashboard.svelte
@@ -18,17 +18,24 @@
   .container {
     display: grid;
     height: 100vh;
+    -moz-transition: .7s cubic-bezier(.45,.67,.9,.73);
+    -webkit-transition: .7s cubic-bezier(.45,.67,.9,.73);
     transition: .7s cubic-bezier(.45,.67,.9,.73);
     /* grid-template-columns: 500px 1fr;  */
   }
 
   aside {
     position: relative;
-    background: rgba(204, 234, 146, 0.514);
+    background: rgba(255, 255, 255, 0.3);
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+    -moz-backdrop-filter: blur(30px);
+    -webkit-backdrop-filter: blur(30px);
+    backdrop-filter: blur(30px);
   }
   
   section {
-    background: rgba(205, 134, 63, 0.442);
+    /* background: rgba(205, 134, 63, 0.442); */
     display: flex;
     justify-content: center;
   }
