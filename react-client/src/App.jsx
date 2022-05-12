26fc0f6b103c88ea163df58383b9918e2bc7aca8
joris-villet <jorisvillet@gmail.com>
2022-05-12 11:14:03 +0200

    route login && register

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import basket from './assets/basket.png';
+import RouterView from './components/RouterView';
 
 export default function App() {
   return (
     <div className="App">
-     
+      <img className="basket" src={basket} alt="image basquette" />
+      <RouterView />
     </div>
   );
 }
